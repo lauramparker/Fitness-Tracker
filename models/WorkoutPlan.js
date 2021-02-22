fae6a5bb250502d28a40d2b27ab941951530c29d
Laura Parker <lauramparker78@gmail.com>
2021-02-22 14:01:53 -0500

    Adding data types to WorkoutSchema (day)

--- models/WorkoutPlan.js
+++ models/WorkoutPlan.js
@@ -3,9 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
+//   name: {   //don't think the workout plan needs a title, just a day and exsercises
+//     type: String,
+//     unique: true
+//   },
+  day: {
+    type: Date,
+    unique: true //there should only be one workoutplan for each day so date needs to be unique
   },
   exercises: [
     {
