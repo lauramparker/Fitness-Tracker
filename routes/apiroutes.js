d2a6de053cb0de6c426b29fd6e8379110f172933
Laura Parker <lauramparker78@gmail.com>
2021-02-23 13:41:31 -0500

    Updating PUT route to add exercises for each workout id

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -30,7 +30,7 @@ app.get("/api/workouts/range", (req, res) => {
 
 //CREATE a NEW workout (date specific)
 app.post("/api/workouts", (req, res) => {
-    db.Workout.create({ day: Date.now() })
+    db.Workout.create({ day: Date.now() }) //adds current date
       .then((dbWorkout) => {
         res.json(dbWorkout);
       })
@@ -40,87 +40,19 @@ app.post("/api/workouts", (req, res) => {
 });
 
 
-//GET a list of exercises
-app.get("/exercise", (req, res) => {
-    db.Exercise.create(req.body)
-      .then((dbExercise) => {
-        res.json(dbExercise);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-});
-
-
-
-    
-//ROUTES for updating SPECIFIC Workouts
-
-//GET a SPECIFIC workout by ID 
-app.get("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(
-      { _id: req.params.id }, { exercises: req.body }
-    ).then((dbWorkout) => {
-      res.json(dbWorkout);
-    }).catch(err => {
-      res.json(err);
-    });
-
-//UPDATE the exercise body for a specific workout by id
-// app.put("/api/workouts/:id", (req, res) => {
-//         db.Workout.findByIdAndUpdate(
-//           { _id: req.params.id }, { exercises: req.body }
-//         ).then((dbWorkout) => {
-//           res.json(dbWorkout);
-//         }).catch(err => {
-//           res.status(400).json(err);
-//         });
-
 app.put("/api/workouts/:id", (req, res) => {
-
     db.Exercise.create(req.body)
-        .then((data) => db.Workout.findOneAndUpdate(
-            { _id: req.params.id },
-            {
-                $push: {
-                    exercises: data._id
-                },
-                $inc: {
-                    totalDuration: data.duration
-                }
-            },
-            { new: true })
-        )
-        .then(dbWorkout => {
+      .then(({_id}) => db.Workout.findOneAndUpdate({ _id: req.params.id }, {$push: {exercises: _id}}, { new: true }))
+                    // $inc: {totalDuration: _id.duration}
+      .then(dbWorkout => {
             res.json(dbWorkout);
         }).catch(err => {
             res.json(err);
         });
 });
 
-// //fill in the exercises into the workout (replacing the id only records)
-// app.get("/api/workouts/populated", (req, res) => {
-//     db.Workout.find({})
-//       .populate("exercises")
-//       .then(dbWorkout => {   //dbWorkout.exercises  to get array of exercises
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//     });
-// });
 
-});
-// //Submit an Exercise to the workout... this is not right... /api/workouts/:workout or /api/workouts/:exercise?
-// app.post("/api/workouts/submit", ({body}, res) => {
-//     db.Exercise.create(body)
-//       .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
-//       .then(dbWorkout => {
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
+}; //end module.export
 
-};
+
+
