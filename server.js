4fe883ff2172a07155f8fafff2080bed63da4cf9
Laura Parker <lauramparker78@gmail.com>
2021-02-23 10:05:37 -0500

    Correcting routes for gettings and creating all workouts per front-end routes

--- server.js
+++ server.js
@@ -18,27 +18,71 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 
-// db.Workout.create({ name: "Workout Plan" }) //date not name
-//   .then(dbWorkout => {
-//     console.log(dbWorkout);
-//   })
-//   .catch(({ message }) => {
-//     console.log(message);
-//   });
-
-
-//create a Workout with thedate when the application starts
-  db.Workout.create({ day: Date }) //not sure this will render correctly
-  .then(dbWorkout => {
-    console.log(dbWorkout);
+//create a Workout Plan Database when the application starts
+db.WorkoutPlan.create({ name: "My Workout Plan" })
+  .then(dbWorkoutPlan => {
+    console.log(dbWorkoutPlan);
   })
   .catch(({ message }) => {
     console.log(message);
   });
 
 
-  //Submit an Exercise to the workout
-  app.post("/submit", ({body}, res) => {
+
+
+//ROUTES for getting, creating WORKOUTS
+
+//GET ALL workouts
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+});
+
+//GET ALL workouts in RANGE
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+});
+
+//CREATE a NEW workout (date specific)
+app.post('/api/workouts', (req, res) => {
+    db.Workout.create(req.body).then((dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+          res.status(400).json(err);
+        });
+    });
+
+
+    
+    
+//ROUTES for updating SPECIFIC Workouts
+
+//fill in the exercises into the workout (replacing the id only records)
+app.get("/populated", (req, res) => {
+    db.Workout.find({})
+      .populate("exercises")
+      .then(dbWorkout => {   //dbWorkout.exercises  to get array of exercises
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+});
+
+
+//Submit an Exercise to the workout... this is not right... /api/workouts/:workout or /api/workouts/:exercise?
+app.post("/api/workouts/submit", ({body}, res) => {
     db.Exercise.create(body)
       .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
       .then(dbWorkout => {
@@ -50,37 +94,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
   });
 
 
-  app.get("/exercises", (req, res) => {
-    db.Exercise.find({})
-      .then(dbExercise => {
-        res.json(dbExercise);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  app.get("/workout", (req, res) => {
-    db.Workout.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  //fill in the exercises into the workout (replacing the id only records)
-  app.get("/populated", (req, res) => {
-    db.Workout.find({})
-      .populate("exercises")
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+
+
   
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
