8f9a6bc3db3885cfdd172968c76322156cbce32d
Laura Parker <lauramparker78@gmail.com>
2021-02-23 09:07:15 -0500

    Drafting basic routes in server.js.  Need to check date-specific workout db

--- server.js
+++ server.js
@@ -16,3 +16,73 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+
+// db.Workout.create({ name: "Workout Plan" }) //date not name
+//   .then(dbWorkout => {
+//     console.log(dbWorkout);
+//   })
+//   .catch(({ message }) => {
+//     console.log(message);
+//   });
+
+
+//create a Workout with thedate when the application starts
+  db.Workout.create({ day: Date }) //not sure this will render correctly
+  .then(dbWorkout => {
+    console.log(dbWorkout);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+
+  //Submit an Exercise to the workout
+  app.post("/submit", ({body}, res) => {
+    db.Exercise.create(body)
+      .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+
+  app.get("/exercises", (req, res) => {
+    db.Exercise.find({})
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.get("/workout", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  //fill in the exercises into the workout (replacing the id only records)
+  app.get("/populated", (req, res) => {
+    db.Workout.find({})
+      .populate("exercises")
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
