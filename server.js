5e4bd4efda1d01d72a4d4e7cda607abd13c70856
Laura Parker <lauramparker78@gmail.com>
2021-02-23 11:07:30 -0500

    Including require routes and require path for html routing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,6 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//require routes
+require("./routes/HTMLroutes.js")(app, path)
+// require("./routes/APIroutes.js")(app)
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
 
@@ -28,7 +33,6 @@ db.Workout.create({ name: "My Workout Plan" })
   });
 
 
-
 //ROUTES for getting, creating WORKOUTS
 
 //GET ALL workouts
@@ -39,7 +43,7 @@ app.get("/api/workouts", (req, res) => {
       })
       .catch(err => {
         res.json(err);
-      });
+    });
 });
 
 //GET ALL workouts in RANGE
@@ -50,24 +54,48 @@ app.get("/api/workouts/range", (req, res) => {
       })
       .catch(err => {
         res.json(err);
-      });
+    });
 });
 
 //CREATE a NEW workout (date specific)
 app.post("/api/workouts", (req, res) => {
-    db.Workout.create(req.body).then((dbWorkout) => {
+    db.Workout.create({ day: Date.now() })
+      .then((dbWorkout) => {
         res.json(dbWorkout);
-      }).catch(err => {
-          res.status(400).json(err);
-        });
-    });
+      })
+      .catch(err => {
+        res.json(err);
+      });
+});
+
+
+//GET a list of exercises
+app.get("/exercise", (req, res) => {
+    db.Exercise.create(req.body)
+      .then((dbExercise) => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+});
 
 
 
     
 //ROUTES for updating SPECIFIC Workouts
 
-// // App.put to update workouts by MongoDB _id value and update the exercise body
+//GET a SPECIFIC workout by ID 
+app.get("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id }, { exercises: req.body }
+    ).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.json(err);
+    });
+
+//UPDATE the exercise body for a specific workout by id
 // app.put("/api/workouts/:id", (req, res) => {
 //         db.Workout.findByIdAndUpdate(
 //           { _id: req.params.id }, { exercises: req.body }
@@ -77,20 +105,41 @@ app.post("/api/workouts", (req, res) => {
 //           res.status(400).json(err);
 //         });
 
+app.put("/api/workouts/:id", (req, res) => {
 
-// //fill in the exercises into the workout (replacing the id only records)
-// app.get("/populated", (req, res) => {
-//     db.Workout.find({})
-//       .populate("exercises")
-//       .then(dbWorkout => {   //dbWorkout.exercises  to get array of exercises
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-// });
+    db.Exercise.create(req.body)
+        .then((data) => db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $push: {
+                    exercises: data._id
+                },
+                $inc: {
+                    totalDuration: data.duration
+                }
+            },
+            { new: true })
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
 
+//fill in the exercises into the workout (replacing the id only records)
+app.get("/api/workouts/populated", (req, res) => {
+    db.Workout.find({})
+      .populate("exercises")
+      .then(dbWorkout => {   //dbWorkout.exercises  to get array of exercises
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+    });
+});
 
+});
 // //Submit an Exercise to the workout... this is not right... /api/workouts/:workout or /api/workouts/:exercise?
 // app.post("/api/workouts/submit", ({body}, res) => {
 //     db.Exercise.create(body)
